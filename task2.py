49fdadcdfcd39f3dd3dfae502ba7a6ea4d5b9824
Mishti Gupta <guptamishti87@gmail.com>
2025-05-26 17:40:20 +0530

    Add only task2.py

---
+++ task2.py
@@ -0,0 +1,5 @@
+first = input('Enter your first name : ')
+last = input('Enter your last name : ')
+full_name = first + ' ' + last 
+print('\n')
+print('Hello,', full_name +'! Welcome to the Python program')
