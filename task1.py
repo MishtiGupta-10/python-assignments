3f70928c76827bfd8fd6c9a9c3e252ee4f66d7c0
MishtiGupta-10 <guptamishti87@gmail.com>
2025-05-26 12:42:26 +0530

    Add only task1.py

---
+++ task1.py
@@ -0,0 +1,14 @@
+a = input('Enter value of number 1 = ')
+b = input('Enter value of number 2 = ')
+a = int(a)
+b = int(b)
+sum = a + b
+diff = a - b
+product = a * b
+quotient = a/b
+remainder = a%b
+print('sum is =',sum)
+print('diference is =',diff)
+print('product is =',product)
+print('quotient is =',quotient)
+print('remainder is =',remainder)
